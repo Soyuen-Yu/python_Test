647ab728d934caf2b23b9c29112a4cdda5b0a6e2
soyuen <lseka82@gmail.com>
2022-04-08 19:47:32 +0900

    182

--- for/for182.py
+++ for/for182.py
@@ -1,2 +1,11 @@
-num = input('Enter the numeber : ')
-
+list_num = ['1','2','3','4','5','6','7','8','9','0','.']
+a = input()
+tmp = a
+for x in range(len(list_num)):
+    a= str(a)
+    a = a.replace(list_num[x],"")
+    
+if a:
+    print(False)
+else:
+    print(tmp)
