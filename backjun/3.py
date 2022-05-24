0b1d59e594cc467cbfe833f8a48f83e40345a37f
soyuen <lseka82@gmail.com>
2022-05-24 15:24:59 +0900

    ex

--- backjun/3.py
+++ backjun/3.py
@@ -3,9 +3,11 @@
 
 문제를 쉽게 만들기 위해 오직 가로와 세로로만 움직일 수 있다고 하자.
 
+
 입력 : 지도의 크기 n과 m이 주어진다. n은 세로의 크기, m은 가로의 크기다.(2 ≤ n ≤ 1000, 2 ≤ m ≤ 1000)
 
 다음 n개의 줄에 m개의 숫자가 주어진다. 0은 갈 수 없는 땅이고 1은 갈 수 있는 땅, 2는 목표지점이다. 입력에서 2는 단 한개이다.
+
 
 출력 : 각 지점에서 목표지점까지의 거리를 출력한다. 원래 갈 수 없는 땅인 위치는 0을 출력하고, 원래 갈 수 있는 땅인 부분 중에서 도달할 수 없는 위치는 -1을 출력한다.
 """
@@ -16,12 +18,12 @@ input = sys.stdin.readline
  
 directions = [(-1, 0), (1, 0), (0, -1), (0, 1)] 
 
-MIIS = lambda: map(int, input().split()) 
+MIIS = lambda: map(int, input().split()) # map : 리스트 안의 요소들을 지정된 함수로 처리해주는 함수로 여기서는 int로 datatype을 변환하는 용도로 사용했다.
 
 directions = [(-1, 0), (1, 0), (0, -1), (0, 1)] 
 
 n, m = MIIS() # n : 세로 m : 가로
-arr = list(list(MIIS()) for _ in range(n))  # map : 리스트 안의 요소들을 지정된 함수로 처리해주는 함수로 여기서는 int로 datatype을 변환하는 용도로 사용했다.
+arr = list(list(MIIS()) for _ in range(n))  
 
 for i in range(n): 
     for j in range(m): 
